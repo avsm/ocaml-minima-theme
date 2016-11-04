333a88959dc2c4bbc00cf2a624afbf8d06ebe635
Anil Madhavapeddy <anil@recoil.org>
2016-11-04 15:28:26 +0000

    also install template files

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -3,7 +3,20 @@
 #require "topkg"
 open Topkg
 
+let tmpl = [
+  "LICENSE.txt";
+  "disqus_comments.html";
+  "footer.html";
+  "google-analytics.html";
+  "head.html";
+  "header.html";
+  "icon-github.html";
+  "icon-github.svg";
+  "icon-twitter.html";
+  "icon-twitter.svg";
+] |> List.map (fun x -> Pkg.share ("_includes/"^x))
+
 let () =
   Pkg.describe "minima-theme" @@ fun c ->
-  Ok [ Pkg.mllib "src/minima_theme.mllib";
-       Pkg.test "test/test"; ]
+  Ok ([ Pkg.mllib "src/minima_theme.mllib";
+       Pkg.test "test/test"; ] @ tmpl)
